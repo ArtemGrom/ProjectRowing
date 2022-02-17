65bbd4a01f9d7284390ce080532881d969b34f93
ArtemGrom <art.gromov@bk.ru>
2022-02-17 23:16:52 +0300

    create db races

--- sql_app/create_db.py
+++ sql_app/create_db.py
@@ -1,5 +1,10 @@
+import sqlite3
+
 import pandas as pd
 import requests
+
+from sql_app.models import Race, RaceBoat
+
 
 competition_id = "ccb6e115-c342-4948-b8e6-4525ff6d7832"  # WorldRowingCup III
 url_races_in_competition = f"https://world-rowing-api.soticcloud.net/stats/api/race/?include=racePhase,event.competition.competitionType,event.competition.competitionType.competitionCategory,event.boatClass&filter[event.competitionId]={competition_id}&sort[date]=asc"
@@ -22,5 +27,7 @@ columns = [
 races_in_competition_df = races_in_competition_df[columns]
 
 # create db all races WRC III
-races_in_competition_df.to_sql(name=models.__tablename__, con=sqlite3.Connection, if_exists="append", index=False)
+conn = sqlite3.connect(Race.__tablename__)
+c = conn.cursor()
+races_in_competition_df.to_sql(name=Race.__tablename__, con=conn, if_exists="append", index=False)
 
