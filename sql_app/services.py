8935974a1676acc48754a3b5b6f47018d168bc63
aeksei <aeksei94@gmail.com>
2022-02-22 13:08:03 +0300

    load RaceBoat data to DB

--- sql_app/services.py
+++ sql_app/services.py
@@ -26,6 +26,12 @@ class AbstractETL(ABC):
     def load(self):
         ...
 
+    @staticmethod
+    def prepare_columns_names(df):
+        df.columns = df.columns.str.replace(".", "")
+        df.columns = df.columns.map(lambda col: col[0].lower() + col[1:])
+
+        return df
 
 class InitDataRaceModel(AbstractETL):
     def __init__(self, competition_id, model):
@@ -60,8 +66,7 @@ class InitDataRaceModel(AbstractETL):
 
         self.races_in_competition_df["DateString"] = pd.to_datetime(self.races_in_competition_df["DateString"], utc=True)
 
-        self.races_in_competition_df.columns = self.races_in_competition_df.columns.str.replace(".", "")
-        self.races_in_competition_df.columns = self.races_in_competition_df.columns.map(lambda col: col[0].lower() + col[1:])
+        self.races_in_competition_df = self.prepare_columns_names(self.races_in_competition_df)
 
     def load(self):
         """Загрузка в уже существующую модель"""
@@ -75,23 +80,49 @@ class InitDataRaceModel(AbstractETL):
 
 
 class InitDataRaceBoatModel(AbstractETL):
-    def __init__(self, competition_id, model):
-        self.competition_id = competition_id
+    def __init__(self, races_id: list[str], model):
+        self.races_id = races_id
         self.model = model
+
+        self.race_boats_df = None
 
         super().__init__()
 
     def extract(self):
         """Загрузка из API"""
-        ...
+        self.race_boats_df = pd.DataFrame()  # пустой датафрейм для
 
-    def trasform(self):
+        for race_id in self.races_id:
+            url_race_boat_final_result = f"https://world-rowing-api.soticcloud.net/stats/api/race/{race_id}?include=racePhase%2CraceBoats.raceBoatAthletes.person%2CraceBoats.invalidMarkResultCode%2CraceBoats.raceBoatIntermediates.distance&sortInclude%5BraceBoats.raceBoatIntermediates.ResultTime%5D=asc"
+
+            resp = requests.get(url_race_boat_final_result)
+            race_boat_final_result = resp.json()["data"]
+
+            self.race_boats_df = self.race_boats_df.append(pd.json_normalize(race_boat_final_result, "raceBoats"))
+
+    def transform(self):
         """Убираем столбцы, при необходимости преобразуем данные"""
-        ...
+        columns = [
+            "id",
+            "raceId",  # Foreign Key Race model
+            "DisplayName",
+            "Rank",
+            "Lane",
+            "ResultTime",
+        ]
+        self.race_boats_df = self.race_boats_df[columns]
+
+        self.race_boats_df = self.prepare_columns_names(self.race_boats_df)
 
     def load(self):
         """Загрузка в уже существующую модель"""
-        ...
+        with engine.begin() as connection:  # https://docs.sqlalchemy.org/en/14/core/connections.html#using-transactions
+            self.race_boats_df.to_sql(
+                self.model.__tablename__,
+                con=connection,
+                if_exists="replace",
+                index=False,
+            )
 
 
 class InitDataRaceBoatIntermediateModel(AbstractETL):
@@ -117,3 +148,7 @@ class InitDataRaceBoatIntermediateModel(AbstractETL):
 if __name__ == '__main__':
     competition = "ccb6e115-c342-4948-b8e6-4525ff6d7832"  # WorldRowingCup III
     races = InitDataRaceModel(competition, model=models.Race)
+
+    races_ids = races.races_in_competition_df["id"]
+    boats = InitDataRaceBoatModel(races_ids, model=models.RaceBoat)
+
